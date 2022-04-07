a96bf2fb622d9e5baac8fe938568b23e8fa96549
rafael.perez.castano <alum.rperezc.2021@iesalixar.org>
2022-04-07 10:51:41 +0200

    Commit ejercicios arrays

--- src/edu/fpdual/ejemplo/ejerciciosArrays/ej4.java
+++ src/edu/fpdual/ejemplo/ejerciciosArrays/ej4.java
@@ -1,7 +1,8 @@
 package edu.fpdual.ejemplo.ejerciciosArrays;
 
 import java.util.Arrays;
-
+//EUC  que contenga un método que reciba un arreglo tipo int y que retorne
+// otro arreglo que contenga los mismos valores del arreglo recibidos por parámetros pero invertidos.
 public class ej4{
 public ej4(){
 
